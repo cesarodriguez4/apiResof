6694a7df727b74e7d9b133ccdeaa8cbec25c0436
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-12-04 22:50:50 -0400

    Apagame la vela maria

--- routes/api.js
+++ routes/api.js
@@ -11,5 +11,27 @@ app.get('/api/recirculado', (req, res) => {
   }, true);
 });
 
+// Devuelve la tabla a comparar linea de liquido
+app.get('/api/diametros/linea_de_liquido', (req, res) => {
+  crud.select(con, {select: '*', from: `sistema_recirculado_congelado`}, (err, row) => {
+    if(err) {
+    	res.send(err);
+    }
+    res.send(row);
+  }, true);
+});
+
+// Devuelve la tabla a comparar linea de liquido
+app.get('/api/diametros/linea_de_succion', (req, res) => {
+  crud.select(con, {select: '*', from: `Tabla_1-6A_RetornoRecirculador`}, (err, row) => {
+    if(err) {
+      res.send(err);
+    }
+    res.send(row);
+  }, true);
+});
+
+
+
 }
 
